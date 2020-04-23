6212f6498884b319ac31de57be24608ed245c14a
julien <julien.calabro@gmail.com>
2020-04-23 16:14:48 +0200

    oui c'est bon

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("ALLLER PTN );
+        System.out.println("ALLLER PTN");
         int a  = 5;
         System.out.println(a);
         //c'est bon c'est gagné
         // oui
+        System.out.println("YES!");
     }
 
 }
