af21bf4995af58e012e774fefec44c9ba7602f8d
LittleSamourai <samiirtiouririne@gmail.com>
2020-04-23 16:13:01 +0200

    DIS MOI QUE C EST BON

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("alleeeer laaa");
+        System.out.println("ALLLER PTN );
         int a  = 5;
         System.out.println(a);
         //c'est bon c'est gagné
